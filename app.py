0c8a0f8c3a1c3537d4d51992da10ed91cecee5b4
Darkdevilsp <b520066@iiit-bh.ac.in>
2024-02-18 14:31:17 +0530

    index
    Signed-off-by: Darkdevilsp <b520066@iiit-bh.ac.in>

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
 from flask import Flask, request, jsonify, send_from_directory
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
 # Load the machine learning model
 with open('modl.pkl', 'rb') as file:
-    modl = pickle.load(file)
+    model = pickle.load(file)
 
 @app.route('/')
 def home():
-    # Assuming your React build files are in the specified directory
-    return send_from_directory(r'C:\Users\srini\OneDrive\Documents\Final Year Project\hospital_management\hospital_management_frontend\src\Pages\HeartDiseasePrediction', 'index.html')
+    # Replace '/path/to/frontend/build' with the actual build folder path
+    return send_from_directory(r'C:\Users\srini\OneDrive\Documents\Final Year Project\hospital_management\hospital_management_frontend\dist', 'index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
     # Extract features from the JSON data
-    int_features = [float(x) for x in data.values()]
+    features = [float(data[key]) for key in data]
     # Prepare the features for prediction
-    final = [np.array(int_features)]
+    input_features = np.array(features).reshape(1, -1)
     # Make predictions using the loaded model
-    prediction = modl.predict(final)
+    prediction = model.predict(input_features)[0]
     # Return the prediction as JSON
-    return jsonify({'prediction': prediction.tolist()})
+    return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
     # Run the Flask app
