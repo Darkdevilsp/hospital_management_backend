bf1d02487e4f635780bc9f4ff6a5dd85631eece5
srinivasroutu <srinivasroutu100@gmail.com>
2024-04-09 12:17:41 +0530

    gcommit

--- ml_model/Heart Disease/app.py
+++ ml_model/Heart Disease/app.py
@@ -1,48 +1,26 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify
 import pickle
 import numpy as np
-import os
-import warnings
-
 
 app = Flask(__name__)
 
 # Load the machine learning model
 with open('modl.pkl', 'rb') as file:
-    model = pickle.load(file)
-
-@app.route('/')
-def home():
-    # Replace '/path/to/frontend/build' with the actual build folder path
-    return send_from_directory(r'C:\Users\srini\OneDrive\Documents\Final Year Project\hospital_management\hospital_management_frontend\dist', 'index.html')
+    modl = pickle.load(file)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the feature values from the form
-    
-    print(request.form)
-    int_features=[float(x) for x in request.form.values()]
-    final=[np.array(int_features)]
-    print(int_features)
-    print(final)
-    prediction=modl.predict(final)
-    #output='(0:{1}f)'.format(prediction[0][1],2)
-  
-    
+    # Get the data from the frontend
+    data = request.json
+
+    # Extract the features from the data
+    features = [float(data[key]) for key in data.keys()]
 
     # Make a prediction using the loaded model
-   
+    prediction = modl.predict([features])
 
-    # Render the prediction result
-    if(prediction==0):
-    #return render_template('result.html', prediction=prediction)
-     return render_template('resultfail.html',prediction=prediction);
-     
-    else :
-       return render_template('resultsucess.html',prediction=prediction);
-    
- 
- 
+    # Return the prediction as JSON response
+    return jsonify({'prediction': prediction.tolist()})
 
 if __name__ == '__main__':
     app.run(debug=True)
